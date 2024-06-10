71de7c441e7eadabd61afc7248e02d52bd4ff899
agustinantonucci <agustinantonucci@gmail.com>
2024-06-10 13:46:41 -0300

    fix: experiencias y actualizaciones

---
+++ src/components/layout/home/Content.js
@@ -0,0 +1,13 @@
+export const Content = () => {
+  return (
+    <div className="experiencias-section">
+      <p className="experiencias-title">
+        <span>
+          Experiencias y <br />
+          actualizaciones
+        </span>
+        <span style={{ color: "var(--red-highlight)" }}> tecnológicas</span>
+      </p>
+    </div>
+  );
+};
--- src/components/layout/home/Home.js
+++ src/components/layout/home/Home.js
@@ -1,12 +1,14 @@
 import { Footer } from "../../ui/footer/Footer";
 import { Header } from "../../ui/header/Header";
 import { Meet } from "../../ui/meet/Meet";
+import { Content } from "./Content";
 import "./home.css";
 
 export const Home = () => {
   return (
     <div className="home-wrapper">
       <Header />
+      <Content />
       <div className="footer-background">
         <Meet />
         <Footer />
